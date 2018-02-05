93f2d3435b8ed9b86073c548785b7cbf4763600d
Laurent Michel <ldm@engr.uconn.edu>
2018-02-05 07:44:16 -0500

    added week1

---
+++ week1/pal1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+   /*
+    * This program is supposed to take (on the standard input) a string (C++)
+    * and produce on its standard output a yes/no answer depending on 
+    * whether the string is indeed a palindrome (reads the same both forward
+    * and backward. 
+    * For this first version, use all the C++ facilities covered so far to 
+    * implement the code.
+    */
+   return 0;
+}
---
+++ week1/pal2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+   /*
+    * This program is supposed to take (on the standard input) a string (C++)
+    * and produce on its standard output a yes/no answer depending on 
+    * whether the string is indeed a palindrome (reads the same both forward
+    * and backward. 
+    * For this second version, stick to plain C to write the code. Make sure 
+    * you are robust against all inputs (length of words). The purpose of the
+    * exercice is to build appreciation for how much C++ automates for you.
+    * Clearly, don't rely on a compile-time fixed array of characters...
+    * Introduce sub-rountines as necessary. 
+    */
+   return 0;
+}
