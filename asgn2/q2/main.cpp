5f5972642bd60a44813ca1d7048034c87e4c7bdb
Joe Sweeney <joseph.sweeney@uconn.edu>
2018-02-18 22:43:44 -0500

    Finish q2

--- asgn2/q2/main.cpp
+++ asgn2/q2/main.cpp
@@ -1,6 +1,15 @@
 #include "wList.H"
+#include <string>
+#include <list>
+#include <iostream>
 
 int main()
 {
-   return 0;
+  using namespace std;
+  auto words = buildList();
+  for(auto w : *words) {
+    cout << w << endl;
+  }
+  delete words;
+  return 0;
 }
--- asgn2/q2/wList.cpp
+++ asgn2/q2/wList.cpp
@@ -1,2 +1,15 @@
+#include <list>
+#include <string>
+#include <iostream>
 #include "wList.H"
 
+using namespace std;
+
+list<string> *buildList() {
+  auto *words = new list<string>;
+  string x;
+  while(cin >> x) {
+    (*words).push_back(x);
+  }
+  return words;
+}
