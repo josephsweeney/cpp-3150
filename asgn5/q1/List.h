e9e65c7789f61933c8ab8390d8f3b7d3200eda70
Joe Sweeney <joseph.sweeney@uconn.edu>
2018-03-07 21:53:02 -0500

    Q1 done

--- asgn5/q1/List.h
+++ asgn5/q1/List.h
@@ -8,21 +8,71 @@ using namespace std;
 template <class T>
 class List {
 public:
-   class Node {
-      friend class List<T>;
-      // TODO
-   };
+  class Node {
+    friend class List<T>;
+    T val;
+    Node *next;
+    Node(T&& v) {val = std::move(v); next = nullptr;}
+    Node(const T& v) {val = v; next = nullptr;}
+
+  /* Node(T&& v) : val(std::move(v)), next(nullptr) {} */
+  /* Node(const T& v) : val(v), next(nullptr) {} */
+
+  };
 private:
    // TODO
 public:
-   List() {}  // TODO
-   ~List() {} // TODO
-   void insert(const T& v) {} // TODO
-   void append(const T& v) {} // TODO
-   void forEach(std::function<void(const T&)> body) {} // TODO
-   // TODO
-   // Recall that you may have to add extra methods beyond the ones
-   // listed here!
+  Node *head;
+  List() { head = nullptr; }
+  ~List() {
+    Node *curr = head;
+    while(curr != nullptr) {
+      Node *n = curr->next;
+      delete curr;
+      curr = n;
+    }
+  }
+  void insert(Node *n) {
+    n->next = head;
+    head = n;
+  }
+  void append(Node *n) {
+    if(head == nullptr) {
+      head = n;
+    }
+    else {
+      Node *curr = head;
+      while(curr->next != nullptr) {curr = curr->next;}
+      curr->next = n;
+    }
+  }
+
+  void insert(const T& v) {
+    insert(new Node(v));
+  }
+  void insert(T&& v) {
+    Node *n = new Node(std::move(v));
+    n->next = head;
+    head = n;
+  }
+
+  void append(const T& v) {
+    append(new Node(v));
+  }
+  void append(T&& v) {
+    append(new Node(std::move(v)));
+  }
+ 
+  void forEach(std::function<void(const T&)> body) {
+    Node *curr = head;
+    while(curr != nullptr) {
+      body(curr->val);
+      curr = curr->next;
+    }
+  }
+  // TODO
+  // Recall that you may have to add extra methods beyond the ones
+  // listed here!
 };
 
 
