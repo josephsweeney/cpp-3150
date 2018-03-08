32d9fa96424f0d64deaadcbf5a0e59da2b931eb1
Joe Sweeney <joseph.sweeney@uconn.edu>
2018-03-07 22:26:56 -0500

    Finish q2

--- asgn5/q2/List.h
+++ asgn5/q2/List.h
@@ -3,19 +3,63 @@
 #include <memory>
 #include <iostream>
 
+// authors: jds13019 rdp12004
+
+using namespace std;
 template <class T>
 class List {
 public:
    class Node {
       friend class List<T>;
-      // TODO (remember that you may have to add quite a few methods!
+      T val;
+      unique_ptr<Node> next;
+      Node(T&& v) {val = std::move(v); next = nullptr;}
+      Node(const T& v) {val = v; next = nullptr;}
    };
 private:
    // TODO
 public:
-   void insert(const T& v) {} // TODO
-   void append(const T& v) {} // TODO
-   void forEach(std::function<void(const T&)> body) {} // TODO
+   unique_ptr<Node> head;
+   List() { head = nullptr; }
+   ~List(){ head = nullptr; }
+   void insert(unique_ptr<Node> n) {
+     n->next = move(head);
+     head = move(n);
+   }
+   void append(unique_ptr<Node> n) {
+     if(head == nullptr) {
+       head = move(n);
+     }
+     else {
+       unique_ptr<Node> *curr = &head;
+       while((*curr)->next != nullptr) {
+	 curr = &(*curr)->next;
+       }
+       (*curr)->next = move(n);
+     }
+   }
+
+   void insert(const T& v) {
+     insert(unique_ptr<Node>(new Node(v)));
+   }
+   void insert(T&& v) {
+     insert(unique_ptr<Node>(new Node(std::move(v))));
+   }
+
+   void append(const T& v) {
+     append(unique_ptr<Node>(new Node(v)));
+   }
+   void append(T&& v) {
+     append(unique_ptr<Node>(new Node(std::move(v))));
+   }
+ 
+   void forEach(std::function<void(const T&)> body) {
+     unique_ptr<Node> *curr = &head;
+     while(*curr != nullptr) {
+       body((*curr)->val);
+       curr = &(*curr)->next;
+     }
+   }
    // TODO
    // Remember that you may have to add methods to meet all the requirements. 
 };
